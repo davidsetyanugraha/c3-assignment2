643f2ee7245946b06d8d9500dbec1628ba86e4dc
Try Ajitiono <ballinst@gmail.com>
2019-04-24 17:19:12 +1000

    Add creds
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- ansible/openrc.sh
+++ ansible/openrc.sh
@@ -24,12 +24,12 @@ unset OS_TENANT_ID
 unset OS_TENANT_NAME
 # In addition to the owning entity (tenant), OpenStack stores the entity
 # performing the action as the **user**.
-export OS_USERNAME="dadingzainul.gusti@student.unimelb.edu.au"
+# export OS_USERNAME="dadingzainul.gusti@student.unimelb.edu.au"
 # With Keystone you pass the keystone password.
 echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
 #read -sr OS_PASSWORD_INPUT
 #export OS_PASSWORD=$OS_PASSWORD_INPUT
-export OS_PASSWORD="Zjc3ZjY2ZjI1ZjkzZjc3"
+# export OS_PASSWORD="Zjc3ZjY2ZjI1ZjkzZjc3"
 # If your configuration has multiple regions, we set that information here.
 # OS_REGION_NAME is optional and only valid in certain environments.
 export OS_REGION_NAME="Melbourne"
--- ansible/run-nectar.sh
+++ ansible/run-nectar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+./nectar-credentials.sh
 . ./openrc.sh; ansible-playbook --ask-become-pass nectar.yaml -vvv
