6b36443756197ff497b0e99296a70467d4768e3a
Try Ajitiono <ballinst@gmail.com>
2019-05-12 13:26:57 +1000

    Edit install nginx script
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- ansible/working_scripts/02-install-nginx.sh
+++ ansible/working_scripts/02-install-nginx.sh
@@ -4,3 +4,7 @@
 # Reinstall.
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y nginx
+
+# Delete the default symbolic link in sites-enabled.
+# Otherwise, our sites isn't the default one listening from port 80.
+sudo rm /etc/nginx/sites-enabled/default
