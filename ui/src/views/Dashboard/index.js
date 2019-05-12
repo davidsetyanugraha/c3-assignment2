0efdb7a9a266ab0ea52db270d35eb0cee6c00e49
Try Ajitiono <ballinst@gmail.com>
2019-05-12 23:44:53 +1000

    Change option to MenuItem and Remove unused import
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- ui/src/views/Dashboard/index.js
+++ ui/src/views/Dashboard/index.js
@@ -13,7 +13,6 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
@@ -37,17 +36,30 @@ function ChartView({ classes }) {
     async function populateData() {
       let url = '';
 
-      if (value === "distance") {
-        url = '/nectar/dashboard_source1/_design/summary/_view/mindistance?group=true';
-      } else if (value === "time") {
-        url = '/nectar/dashboard_source1/_design/summary/_view/mintimediff?group=true';
-      } else if (value === "sins") {
-        url = '/nectar/dashboard_source1/_design/summary/_view/sumsins?group=true';
-      } else if (value === "liveable") {
-        url = '/nectar/dashboard_source1/_design/summary/_view/mindistance?group=true';
+      if (value === 'distance') {
+        url =
+          '/nectar/dashboard_source1/_design/summary/_view/mindistance?group=true';
+      } else if (value === 'time') {
+        url =
+          '/nectar/dashboard_source1/_design/summary/_view/mintimediff?group=true';
+      } else if (value === 'sins') {
+        url =
+          '/nectar/dashboard_source1/_design/summary/_view/sumsins?group=true';
+      } else if (value === 'liveable') {
+        url =
+          '/nectar/dashboard_source1/_design/summary/_view/mindistance?group=true';
       }
-      console.log("set new mapped data");
-      console.log("value = " + value + " ,direction = " + direction + " ,level = " + level + " ,specific = " + specific );
+      console.log('set new mapped data');
+      console.log(
+        'value = ' +
+          value +
+          ' ,direction = ' +
+          direction +
+          ' ,level = ' +
+          level +
+          ' ,specific = ' +
+          specific
+      );
 
       const response = await fetch(url);
       const jres = await response.json();
@@ -59,14 +71,12 @@ function ChartView({ classes }) {
           value: json[key]
         };
       });
-      mappedData.sort( compare );
+      mappedData.sort(compare);
       mappedData.reverse();
-      mappedData = getNFirst(mappedData, 5);//
+      mappedData = getNFirst(mappedData, 5); //
 
-      console.log("new mapped data");
+      console.log('new mapped data');
       console.log(mappedData);
-      
-
 
       setData(mappedData);
     }
@@ -86,24 +96,24 @@ function ChartView({ classes }) {
     function processData(json) {
       let results = [];
 
-      json.forEach((e) => {
+      json.forEach(e => {
         let from = e.key[0];
         let to = e.key[1];
         let val;
-        
-        if (value === "distance") {
-          val = e.value.min; 
-        } else if (value === "time") {
-          val = e.value.min; 
-        } else if (value === "sins") {
-          val = e.value.sum; 
-        } else if (value === "liveable") {
-          val = e.value.sum; 
+
+        if (value === 'distance') {
+          val = e.value.min;
+        } else if (value === 'time') {
+          val = e.value.min;
+        } else if (value === 'sins') {
+          val = e.value.sum;
+        } else if (value === 'liveable') {
+          val = e.value.sum;
         }
 
-        if ((direction === "from") & (from === specific)) {
+        if ((direction === 'from') & (from === specific)) {
           results[to] = val;
-        } else if ((direction === "to") & (to === specific)){
+        } else if ((direction === 'to') & (to === specific)) {
           results[from] = val;
         }
       });
@@ -111,147 +121,153 @@ function ChartView({ classes }) {
       return results;
     }
 
-    function compare( a, b ) {
-      if ( a.value < b.value ){
+    function compare(a, b) {
+      if (a.value < b.value) {
         return -1;
       }
-      if ( a.value > b.value ){
+      if (a.value > b.value) {
         return 1;
       }
       return 0;
     }
 
     populateData();
-
-  }, [direction,level,specific,value]);
+  }, [direction, level, specific, value]);
 
   const [location, setLocation] = useState([]);
 
   useEffect(() => {
     async function fetchLocations(direction, level) {
-      let url = "";
-      if ((direction === "from") & (level === "city")) {
-        url = '/nectar/dashboard_source1/_design/summary/_view/fromCity?group=true';
-      } else if ((direction === "to") & (level === "city")) {
-        url = '/nectar/dashboard_source1/_design/summary/_view/toCity?group=true';
+      let url = '';
+      if ((direction === 'from') & (level === 'city')) {
+        url =
+          '/nectar/dashboard_source1/_design/summary/_view/fromCity?group=true';
+      } else if ((direction === 'to') & (level === 'city')) {
+        url =
+          '/nectar/dashboard_source1/_design/summary/_view/toCity?group=true';
       }
 
-      console.log("calling: " + url);
+      console.log('calling: ' + url);
       const response = await fetch(url);
       if (response.ok) {
-        const json = await response.json();        
+        const json = await response.json();
         const data = json.rows;
         setSpecific(data[0].key);
         setLocation(data);
       } else {
-        alert("HTTP-Error: " + response.status);
+        alert('HTTP-Error: ' + response.status);
       }
     }
 
-    fetchLocations(direction,level);
-  }, [direction,level]);
+    fetchLocations(direction, level);
+  }, [direction, level]);
 
   function generateLocations() {
     // let sampleData = [
     //   {"key":"YARRIAMBIACK","value":2}
     //   ];
 
-    let locations = [];  
+    let locations = [];
 
-    if ((direction === "from") & (level === "city")) {
-      location.forEach((e) => {
-        locations.push(<option key= {e.key} value={e.key}> {e.key} </option>);
+    if ((direction === 'from') & (level === 'city')) {
+      location.forEach(e => {
+        locations.push(
+          <MenuItem key={e.key} value={e.key}>
+            {' '}
+            {e.key}{' '}
+          </MenuItem>
+        );
       });
-    } else if ((direction === "to") & (level === "city")) {
-      location.forEach((e) => {
-        locations.push(<option key= {e.key} value={e.key}> {e.key} </option>);
+    } else if ((direction === 'to') & (level === 'city')) {
+      location.forEach(e => {
+        locations.push(
+          <MenuItem key={e.key} value={e.key}>
+            {' '}
+            {e.key}{' '}
+          </MenuItem>
+        );
       });
     }
 
     return locations;
-  }  
+  }
 
   return (
     <div>
       <h2>Dashboard</h2>
-      <form className={classes.form} onSubmit={handleSubmit}>
-        <Grid container spacing={16}>
-          <Grid item xs={2}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="direction">Direction</InputLabel>
-              <Select
-                value={direction}
-                onChange={e => setDirection(e.target.value)}
-                inputProps={{
-                  name: 'direction',
-                  id: 'direction'
-                }}
-              >
-                <MenuItem value="from">From</MenuItem>
-                <MenuItem value="to">To</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={2}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="location-level">Location Level</InputLabel>
-              <Select
-                value={level}
-                onChange={e => setLevel(e.target.value)}
-                inputProps={{
-                  name: 'location-level',
-                  id: 'location-level'
-                }}
-              >
-                <MenuItem value="city">City</MenuItem>
-                <MenuItem value="street">Street</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={2}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="specific-location">
-                Specific Location
-              </InputLabel>
-              <Select
-                value={specific}
-                onChange={e => setSpecific(e.target.value)}
-                inputProps={{
-                  name: 'specific-location',
-                  id: 'specific-location'
-                }}
-              >
-                {generateLocations()}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={2}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="value">Value</InputLabel>
-              <Select
-                value={value}
-                onChange={e => setValue(e.target.value)}
-                inputProps={{
-                  name: 'specific-location',
-                  id: 'specific-location'
-                }}
-              >
-                <MenuItem value="distance">Distance</MenuItem>
-                <MenuItem value="time">Time</MenuItem>
-                <MenuItem value="sins">Sins</MenuItem>
-                <MenuItem value="liveable">Liveable</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={2}>
-            <Button variant="outlined" type="submit" value="Submit">
-              Get Data
-            </Button>
-          </Grid>
+      <Grid container spacing={16} className={classes.form}>
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="direction">Direction</InputLabel>
+            <Select
+              value={direction}
+              onChange={e => setDirection(e.target.value)}
+              inputProps={{
+                name: 'direction',
+                id: 'direction'
+              }}
+            >
+              <MenuItem value="from">From</MenuItem>
+              <MenuItem value="to">To</MenuItem>
+            </Select>
+          </FormControl>
         </Grid>
-      </form>
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="location-level">Location Level</InputLabel>
+            <Select
+              value={level}
+              onChange={e => setLevel(e.target.value)}
+              inputProps={{
+                name: 'location-level',
+                id: 'location-level'
+              }}
+            >
+              <MenuItem value="city">City</MenuItem>
+              <MenuItem value="street">Street</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="specific-location">
+              Specific Location
+            </InputLabel>
+            <Select
+              value={specific}
+              onChange={e => setSpecific(e.target.value)}
+              inputProps={{
+                name: 'specific-location',
+                id: 'specific-location'
+              }}
+            >
+              {generateLocations()}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="value">Value</InputLabel>
+            <Select
+              value={value}
+              onChange={e => setValue(e.target.value)}
+              inputProps={{
+                name: 'specific-location',
+                id: 'specific-location'
+              }}
+            >
+              <MenuItem value="distance">Distance</MenuItem>
+              <MenuItem value="time">Time</MenuItem>
+              <MenuItem value="sins">Sins</MenuItem>
+              <MenuItem value="liveable">Liveable</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
 
-      <h2>Top 5 {level} {direction} {specific} by {value}</h2>
+      <h2>
+        Top 5 {level} {direction} {specific} by {value}
+      </h2>
 
       <div className={classes.chart}>
         <BarChart
